0e7cf76bb63596656a04b148b5426e2e988967a2
hyeyoung0112 <hyeyoung0112@gmail.com>
2019-01-17 22:41:33 +0900

    start!

---
+++ app/src/main/java/com/example/q/googlestreetview/StreetViewActivity.java
@@ -0,0 +1,13 @@
+package com.example.q.googlestreetview;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class StreetViewActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_street_view);
+    }
+}
